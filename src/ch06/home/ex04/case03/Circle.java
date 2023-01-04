7f2c3eae5a7046e782230cf76d8f302522b22eb1
최한석 <a@a.com>
2023-01-04 16:10:08 +0900

    a

---
+++ src/ch06/home/ex04/case03/Circle.java
@@ -0,0 +1,17 @@
+package ch06.home.ex04.case03;
+
+public class Circle {
+	private Point point;
+	private int r;
+	
+	//과제: 위 compile error를 해결토록, 생성자를 기술하라.
+	public Circle(Point point, int r) {
+		this.point = point;
+		this.r = r;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s, %d", this.point, this.r);
+	}
+}
