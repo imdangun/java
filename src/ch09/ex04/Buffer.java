4cd68a7f89821e7e9a5972b98459868abe5cf4a4
최한석 <a@a.com>
2023-01-11 11:13:10 +0900

    a

---
+++ src/ch09/ex04/Buffer.java
@@ -0,0 +1,30 @@
+package ch09.ex04;
+
+public class Buffer {
+	private Integer val;
+	
+	public synchronized int remove() {
+		while(val == null) {
+			try {
+				wait();
+			} catch(InterruptedException e) {}
+		}
+		
+		int val = this.val;
+		this.val = null;		
+		notifyAll();
+		
+		return val;
+	}
+	
+	public synchronized void add(Integer val) {
+		while(this.val != null) {
+			try {
+				wait();
+			} catch(InterruptedException e) {}
+		}
+		
+		this.val = val;
+		notifyAll();		
+	}
+}
